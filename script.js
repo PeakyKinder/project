293536a55b19b0b8000fbc9e7b20eade59297461
PeakyKinder <stethem707@gmail.com>
2022-07-31 09:44:58 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4 / 0);
+console.log('string' * 9)
+
+let persone = {
+	name: "John",
+	age: 25,
+	isMarried: true
+};
+
+console.log(persone.name);
+console.log(persone.age)
+console.log(persone.isMarried)
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp']
+
+console.log(arr[0])
+
+// alert('Hello world');
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+
+//let answer = +prompt("Есть ли вам 18?", "Да");
+
+//console.log(typeof (answer));
+
+//console.log("arr" + " - object");
+//console.log(5 + " - object");
+
+let incr = 10,
+	decr = 10;
+
+
+incr++;
+decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log("3" == 2);
+
+let isChecked = false;
+	isClose = true;
+
+console.log(isChecked || isClose);
+
